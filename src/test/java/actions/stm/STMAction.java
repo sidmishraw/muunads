c6540137a303e5c51ee2a728e4027a7b82ead6ae
Sidharth Mishra <sidmishraw@gmail.com>
2017-12-23 20:27:16 -0800

    Added STM action.

---
+++ src/test/java/actions/stm/STMAction.java
@@ -0,0 +1,72 @@
+/**
+ * Project: Muunads
+ * Package: actions.stm
+ * File: STMAction.java
+ * 
+ * @author sidmishraw
+ *         Last modified: Dec 23, 2017 1:07:57 PM
+ */
+package actions.stm;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import muunads.Monad;
+
+/**
+ * A STM action. To be used as the actions inside the Transactions.
+ * 
+ * @author sidmishraw
+ *
+ *         Qualified Name: actions.stm.STMAction
+ *
+ */
+public class STMAction<A> implements Monad<A> {
+    
+    /**
+     * The action performed when the STMAction is executed.
+     */
+    private Supplier<A> action;
+    
+    /**
+     * Creates a new STMAction with the specified action.
+     * 
+     * @param action
+     *            the action to be performed when executing the STMAction.
+     */
+    public STMAction(Supplier<A> action) {
+        this.action = action;
+    }
+    
+    /*
+     * Wraps `a` in a Supplier (function) delaying the action. Gives back a new STMAction, which when performed returns
+     * `a`.
+     * 
+     * @see muunads.Monad#wrap(java.lang.Object)
+     */
+    @Override
+    public Monad<A> wrap(A a) {
+        return new STMAction<>(() -> a); // delayed action returns `a`
+    }
+    
+    /*
+     * First executes the current STMAction, the result is then shoved into the function `fromAToMonadOfB` and the
+     * resulting Monad is returned.
+     * 
+     * @see muunads.Monad#bind(java.util.function.Function)
+     */
+    @Override
+    public <C extends Monad<B>, B> C bind(Function<A, C> fromAToMonadOfB) {
+        A actionResult = this.perform();
+        return fromAToMonadOfB.apply(actionResult);
+    }
+    
+    /**
+     * Performs the STMAction and returns any result of the action.
+     * 
+     * @return the result obtained after performing the STMAction.
+     */
+    public A perform() {
+        return this.action.get();
+    }
+}
